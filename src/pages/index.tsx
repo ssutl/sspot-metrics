353f078a036b0f4b86b82a5b2d306b9d89078026
Shay Campbell <shayc.campbell@gmail.com>
2024-07-04 17:26:15 +0100

    Mobile view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,6 +30,7 @@ interface ChartData {
 export default function Home() {
   const [chartData, setChartData] = useState<ChartData[]>([]);
   const [potIsLive, setPotIsLive] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
 
   const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -39,6 +40,23 @@ export default function Home() {
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const db = getDatabase(app);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    // Initial check
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     const plantRef = ref(db, "SSPOTV1");
@@ -100,85 +118,105 @@ export default function Home() {
         </span>{" "}
         SSPOTV1
       </h1>
-      <ResponsiveContainer width="100%" height={700}>
-        <AreaChart
-          data={chartData}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <defs>
-            <linearGradient id="colorConductivity" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor={colour1} stopOpacity={0.8} />
-              <stop offset="95%" stopColor={colour1} stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id="colorTemperature" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor={colour2} stopOpacity={0.95} />
-              <stop offset="95%" stopColor={colour2} stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id="colorMoisture" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor={colour3} stopOpacity={0.8} />
-              <stop offset="95%" stopColor={colour3} stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis
-            dataKey="time"
-            tickFormatter={formatXAxis}
-            label={{
-              value: "Time (dd/MM HH:mm)",
-              position: "insideBottomRight",
-              offset: -5,
+      {isMobile ? (
+        <>
+          {/** write the moisture temp and conductivity in p */}
+          <p>
+            Conductivity: {chartData[chartData.length - 1]?.Conductivity} us/cm
+          </p>
+          <p>Moisture: {chartData[chartData.length - 1]?.Moisture}%</p>
+          <p>Temperature: {chartData[chartData.length - 1]?.Temperature}°C</p>
+          <p>
+            Graph is not supported on mobile. Please view on a larger screen.
+          </p>
+        </>
+      ) : (
+        <ResponsiveContainer width="100%" height={700}>
+          <AreaChart
+            data={chartData}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
             }}
-          />
-          <YAxis
-            yAxisId="left"
-            label={{
-              value: "Conductivity (us/cm)",
-              angle: -90,
-              position: "insideLeft",
-            }}
-          />
-          <YAxis
-            yAxisId="right"
-            orientation="right"
-            label={{
-              value: "Moisture (%) & Temperature (°C)",
-              angle: 90,
-              position: "insideRight",
-            }}
-          />
-          <Tooltip />
-          <Legend />
-          <Area
-            type="monotone"
-            dataKey="Conductivity"
-            stroke={colour1}
-            fill="url(#colorConductivity)"
-            fillOpacity={0.5}
-            yAxisId="left"
-          />
-          <Area
-            type="monotone"
-            dataKey="Temperature"
-            stroke={colour2}
-            fill="url(#colorTemperature)"
-            fillOpacity={0.5}
-            yAxisId="right"
-          />
-          <Area
-            type="monotone"
-            dataKey="Moisture"
-            stroke={colour3}
-            fill="url(#colorMoisture)"
-            fillOpacity={0.5}
-            yAxisId="right"
-          />
-        </AreaChart>
-      </ResponsiveContainer>
+          >
+            <defs>
+              <linearGradient
+                id="colorConductivity"
+                x1="0"
+                y1="0"
+                x2="0"
+                y2="1"
+              >
+                <stop offset="5%" stopColor={colour1} stopOpacity={0.8} />
+                <stop offset="95%" stopColor={colour1} stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorTemperature" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor={colour2} stopOpacity={0.95} />
+                <stop offset="95%" stopColor={colour2} stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorMoisture" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor={colour3} stopOpacity={0.8} />
+                <stop offset="95%" stopColor={colour3} stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="time"
+              tickFormatter={formatXAxis}
+              label={{
+                value: "Time (dd/MM HH:mm)",
+                position: "insideBottomRight",
+                offset: -5,
+              }}
+            />
+            <YAxis
+              yAxisId="left"
+              label={{
+                value: "Conductivity (us/cm)",
+                angle: -90,
+                position: "insideLeft",
+              }}
+            />
+            <YAxis
+              yAxisId="right"
+              orientation="right"
+              label={{
+                value: "Moisture (%) & Temperature (°C)",
+                angle: 90,
+                position: "insideRight",
+              }}
+            />
+            <Tooltip />
+            <Legend />
+            <Area
+              type="monotone"
+              dataKey="Conductivity"
+              stroke={colour1}
+              fill="url(#colorConductivity)"
+              fillOpacity={0.5}
+              yAxisId="left"
+            />
+            <Area
+              type="monotone"
+              dataKey="Temperature"
+              stroke={colour2}
+              fill="url(#colorTemperature)"
+              fillOpacity={0.5}
+              yAxisId="right"
+            />
+            <Area
+              type="monotone"
+              dataKey="Moisture"
+              stroke={colour3}
+              fill="url(#colorMoisture)"
+              fillOpacity={0.5}
+              yAxisId="right"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      )}
     </main>
   );
 }
